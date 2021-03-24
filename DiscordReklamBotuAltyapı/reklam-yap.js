33c43d252050d13d233e30e38b5b056c28e6d4c1
phoenixAdam <81285510+phoenixAdam@users.noreply.github.com>
2021-03-24 06:58:29 +0000

    Add files via upload

---
+++ DiscordReklamBotuAltyapı/reklam-yap.js
@@ -0,0 +1,5 @@
+const.Discord = ('discord.js')
+exports.run = (client, message, args)
+let link = message.link
+if (!link) return.message.channel.send('ERR_001 | Linksiz Reklam Hatasý | Lütfen Bir Link Koyunuz')
+if (link) return.message.channel.sent('**REKLAM** : message.link')
